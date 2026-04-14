443cd1823fa023aee1f49805ca396e5fcb2d661f
Gabyface <gabyface67@gmail.com>
2026-04-14 07:03:33 -0400

    Add install script for Avalanche 1.0.0

---
+++ commands/install.sh
@@ -0,0 +1,5 @@
+# SH SCRIPT TO INSTALL AVALANCHE
+
+echo "Installing Avalanche 1.0.0..."
+git clone https://github.com/Gabyface910/avalanche /$HOME/.avalanche
+sudo ln /$HOME/.avalanche/commands/main.sh /usr/local/bin/avalanche
